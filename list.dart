4c0fe68fa8ce871b52e57009dac58e25182d6ddb
paragkumbhar79 <140073768+paragkumbhar79@users.noreply.github.com>
2023-07-20 18:51:55 +0530

    Update list.dart

--- list.dart
+++ list.dart
@@ -1,63 +1,42 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class ListWidget extends StatefulWidget {
+class ListWidget extends StatelessWidget {
   const ListWidget({super.key});
-
-  @override
-  State<ListWidget> createState() => _ListWidgetState();
-}
-
-class _ListWidgetState extends State<ListWidget> {
-
-
-
   @override
   Widget build(BuildContext context) {
-
     var list = <Widget>[];
 
     for (var i = 0; i <= 100; i++) {
-      list.add(const ListItemWidget());
+
+      list.add(ListItemWidget(count: 0,index: i,));
+
     }
     return ListView.builder(itemCount: list.length,itemBuilder: (context,index)=> list[index]);
   }
 }
 
 class ListItemWidget extends StatefulWidget {
-  const ListItemWidget({super.key});
+  int? index;
+  int? count;
+  ListItemWidget({super.key,this.count,this.index});
   @override
   State<ListItemWidget> createState() => _ListItemWidgetState();
 }
 
 
 class _ListItemWidgetState extends State<ListItemWidget> {
-  int tap=0;
-  var count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.all(4),
         child: Row(
           children: [
-            RichText(text: TextSpan(
-              recognizer: new TapGestureRecognizer()
-                ..onTap = () {
-                  setState(() {
-                    tap++;
-                  });
-                  if(tap==3){
-                    count++;
-                  }
-                },
-              text: count.toString(),
-            )),
-            //Text(count.toString()),
-            SizedBox(width: 10,),
+            Text(widget.count.toString()),
             MaterialButton(
               onPressed: () {
                 setState(() {
-                  count++;
+                  widget.count=widget.count!+1;
                 });
               },
               child: const Text("+"),
